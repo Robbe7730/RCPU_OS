f77af04da78961d2f45a8ae2de5b44910b86a388
Robbe Van Herck <robbe@robbevanherck.be>
2020-11-22 16:16:18 +0100

    Hello, world! But _even_ better

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,15 @@
 use core::panic::PanicInfo;
 
 extern crate volatile;
+extern crate lazy_static;
+extern crate spin;
 
 mod terminal;
 
 // The new entry point
 #[no_mangle] // Disables mangling the name of _start
 pub extern "C" fn _start() -> ! {
-    terminal::print_something();
+    println!("Hello RCPU_{}S", 0);
 
     loop{}
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,4 +1,16 @@
-use volatile::Volatile;
+use lazy_static::lazy_static; // So we can have a static WRITER
+use spin::Mutex;              // So the static WRITER can't have data races
+use volatile::Volatile;       // For the Buffer
+use core::fmt;                // For the write! and writeln! macros
+
+// "Static" Writer instance for writing globally
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::LightGray, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
 
 #[allow(dead_code)]                          // Some are unused and that is ok
 #[derive(Debug, Clone, Copy, PartialEq, Eq)] // Derive the implementations
@@ -77,7 +89,26 @@ impl Writer {
         }
     }
 
-    fn new_line(&mut self) {/* TODO */}
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+    
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
@@ -92,17 +123,27 @@ impl Writer {
     }
 }
 
-pub fn print_something() {
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
 
-    writer.write_string("Hello ");
-    writer.write_byte(b'R');
-    writer.write_byte(b'C');
-    writer.write_byte(b'P');
-    writer.write_byte(b'U');
-    writer.write_string("_OS!");
+// print!() macro
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::terminal::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
 }
