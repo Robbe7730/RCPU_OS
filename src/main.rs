f053f15215ec76f7be21142b48a65aa1da53288c
Robbe Van Herck <robbe@robbevanherck.be>
2020-11-22 15:21:32 +0100

    Hello World!

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,26 @@
 
 use core::panic::PanicInfo;
 
-// Function that gets called on panic
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop{}
-}
+
+static HELLO: &[u8] = b"Hello RCPU_OS";
 
 // The new entry point, "no_mangle" disables mangling the name of _start
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x0F;
+        }
+    }
+
+    loop{}
+}
+
+// Function that gets called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
