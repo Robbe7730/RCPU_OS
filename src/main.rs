c9780c38dfa7daedc60e492846ed39fa59584c3d
Robbe Van Herck <robbe@robbevanherck.be>
2020-11-22 14:56:13 +0100

    Basic freestanding binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello world!");
+// Disable standard library and "regular" entry point
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// Function that gets called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
+
+// The new entry point, "no_mangle" disables mangling the name of _start
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop{}
 }
