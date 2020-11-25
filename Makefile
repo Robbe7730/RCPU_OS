f00400324208bc0175e98dab8fc5a52790af8b2f
Robbe Van Herck <robbe@robbevanherck.be>
2020-11-25 14:11:47 +0100

    Rewrite using steps from the first version to get multiboot2 and module support

---
+++ Makefile
@@ -0,0 +1,45 @@
+ARCH ?= x86_64
+TARGET ?= $(ARCH)-rcpu_os
+
+KERNEL := build/kernel-$(ARCH).bin
+ISO := build/rcpu_os-$(ARCH).iso
+RUST_OS := target/$(TARGET)/debug/librcpu_os.a
+
+LINKER_SCRIPT := src/arch/$(ARCH)/linker.ld
+GRUB_CFG := src/arch/$(ARCH)/grub.cfg
+ASM_SRC_FILES := $(wildcard src/arch/$(ARCH)/*.asm)
+ASM_OBJ_FILES := $(patsubst src/arch/$(ARCH)/%.asm, \
+	build/arch/$(ARCH)/%.o, $(ASM_SRC_FILES))
+
+
+.PHONY: all clean run iso kernel
+
+all: $(KERNEL)
+
+clean:
+	rm -r build target
+
+run: $(ISO)
+	qemu-system-x86_64 -cdrom $(ISO)
+
+iso: $(ISO)
+
+$(ISO): $(KERNEL) $(GRUB_CFG)
+	mkdir -p build/isofiles/boot/grub
+	cp modules/* build/isofiles/boot
+	cp $(KERNEL) build/isofiles/boot/kernel.bin
+	cp $(GRUB_CFG) build/isofiles/boot/grub
+	grub-mkrescue -o $(ISO) build/isofiles
+	rm -r build/isofiles
+
+$(KERNEL): kernel $(RUST_OS) $(ASM_OBJ_FILES) $(LINKER_SCRIPT)
+	ld -n -T $(LINKER_SCRIPT) -o $(KERNEL) \
+		$(ASM_OBJ_FILES) $(RUST_OS)
+
+kernel:
+	RUST_TARGET_PATH=$(shell pwd) cargo build --target $(TARGET)
+
+# compile assembly files
+build/arch/$(ARCH)/%.o: src/arch/$(ARCH)/%.asm
+	mkdir -p $(shell dirname $@)
+	nasm -felf64 $< -o $@
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,54 @@
-// Disable standard library
+#![feature(lang_items)]
+#![feature(abi_x86_interrupt)]
 #![no_std]
 
-// Enable the "x86-interrupt" calling convention
-#![feature(abi_x86_interrupt)]
-
+extern crate multiboot2;
 extern crate x86_64;
 extern crate volatile;
-extern crate lazy_static;
 extern crate spin;
+extern crate lazy_static;
 extern crate pic8259_simple;
+extern crate pc_keyboard;
 
-pub mod terminal;
-pub mod interrupts;
-pub mod gdt;
+mod terminal;
+mod interrupts;
+mod gdt;
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern fn rust_main(multiboot_information_address: usize) {
+    println!("Hello RCPU_{}S", 1);
+
+    init();
+
+    println!("multiboot_information_address: {}", multiboot_information_address);
+
+    let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
+    let module_tags = boot_info.module_tags();
+
+    println!("Modules available:");
+    for module_tag in module_tags {
+        println!("{}: {}-{}", module_tag.name(), module_tag.start_address(),
+            module_tag.end_address());
+    }
+
+    hlt_loop();
+}
 
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
     unsafe { interrupts::PICS.lock().initialize() };
     x86_64::instructions::interrupts::enable();
+
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    hlt_loop();
 }
 
 pub fn hlt_loop() -> ! {
@@ -26,3 +56,4 @@ pub fn hlt_loop() -> ! {
         x86_64::instructions::hlt();
     }
 }
+
